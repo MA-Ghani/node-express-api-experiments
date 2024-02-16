6e8871bd51e2d64a10abe200ab42477f51bf355c
MAGhani <ghani.rajput@gmail.com>
2024-02-16 17:08:14 +0100

    changes

--- routes/api/autotelex/createdata/autoTelex.controller.js
+++ routes/api/autotelex/createdata/autoTelex.controller.js
@@ -3,19 +3,21 @@ const autoTelexModel = require("../../../../models/autoTelex");
 exports.createData = async (req, res, next) => {
   try {
     const { incoming } = req.body;
-	
+
     //Validate and check if incoming field is an object and not empty
-	if (typeof incoming !== "object" || !Object.keys(incoming).length) {
-	  return res.status(400).json({ error: "Incoming Data in required" });
-	}
-	console.log("incoming <<<<<<<<<<<<<<<<<<<<<<<<")
-	console.log(incoming)
+    if (typeof incoming !== "object" || !Object.keys(incoming).length) {
+      return res.status(400).json({ error: "Incoming Data in required" });
+    }
+
     //Add Media
     const data = new autoTelexModel({
       incoming,
     });
 
     await data.save();
+
+    console.log("incoming <<<<<<<<<<<<<<<<<<<<<<<<");
+    console.log(incoming);
 
     res.status(200).json(data);
     return;
